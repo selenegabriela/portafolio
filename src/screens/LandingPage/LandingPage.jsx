501d7ff1a09cb6dfbfc16dc3a9a3ae348e36dfca
Selene Gabriela Amador Díaz <sel_nn@hotmail.com>
2022-11-08 12:54:53 -0600

    structure and Landing page

--- src/screens/LandingPage/LandingPage.jsx
+++ src/screens/LandingPage/LandingPage.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './LandingPage.module.css';
+import laptopImage from '../../images/laptop.png'
+import nameImage from '../../images/selenegabriela.svg'
+
 const Home = () => {
 
   return (
-    <div className={s.div}>
+    <div>
       <div>
-        <p>"Quien quiera ser feliz que lo sea: del mañana no hay certeza".</p>
-        <span>-Lorenzo de' Medici</span>
+        <div className={s.flexNameImage}>
+          <div>
+            <p>Hola,</p>
+            <p>Mi nombre es </p>
+            <img className={s.name} src={nameImage} alt="name" />
+          </div>
+          <div>
+            <img src={laptopImage} alt="laptop-icon"/>
+          </div>
+        </div>
+        <div className={s.positionButton}>
+          <p>Fullstack web developer</p>
+          <NavLink to='/home'>
+            <button>Continuar</button>
+          </NavLink>
+        </div>
+        
       </div>
       <div>
-        <NavLink to='/home'>
-          <button>Continuar</button>
-        </NavLink>
       </div>
     </div>
   )
