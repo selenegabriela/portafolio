5e77000b6bd5bf1d960e2fa8f94223a68e6f14f0
Selene Gabriela Amador Díaz <sel_nn@hotmail.com>
2022-11-18 20:55:19 -0600

    navbar options CV

--- src/Sections/NavBar/NavBar.jsx
+++ src/Sections/NavBar/NavBar.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './NavBar.module.css'
 import github from '../../images/github.svg'
 import whatsapp from '../../images/whatsapp.svg'
 import linkedin from '../../images/linkedin.svg'
 
 const NavBar = () => {
+
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleShowOptions = () => {
+    setShowOptions(!showOptions);
+  }
+
   return (
     <div className={s.navbar}>
-        <img className={s.iconsContact} src={github} alt="github" />
-        <img className={s.iconsContact} src={whatsapp} alt="whatsapp" />
-        <img className={s.iconsContact} src={linkedin} alt="linkedin" />
+      <div className={s.btnAndOptions}>
+        <button onClick={handleShowOptions} className={s.btnCv}>
+          <hr />
+          <hr />
+          <hr />
+        </button>
+        <div className={!showOptions ? s.hide : ''}>
+          <a className={s.options} href='#'>
+            <p>Home</p>
+          </a>
+          <a className={s.options} href='#'>
+            <p>Download CV in English</p>
+          </a>
+          <a className={s.options} href='#'>
+            <p>Download CV in Spanish</p>
+          </a>
+        </div>
+      </div>
+      <div className={s.navbarIcons}>
+        <a target='_blank' rel='noreferrer' href="https://github.com/selenegabriela">
+          <img className={s.iconsContact} src={github} alt="github" />
+        </a>
+        <a target='_blank' rel='noreferrer' href="https://wa.me/qr/J2ZJCB52XXIVE1">
+          <img className={s.iconsContact} src={whatsapp} alt="whatsapp" />
+        </a>
+        <a target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/selenegabriela/">
+          <img className={s.iconsContact} src={linkedin} alt="linkedin" />
+        </a>
+      </div>
 
     </div>
   )
